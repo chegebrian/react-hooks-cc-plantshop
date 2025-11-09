838cc90c71eb804e38078d371f04a29c97dad13c
brianwaithaka <waithakabrian8@gmail.com>
2025-11-09 14:10:48 +0300

    handle stock out functionality

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ name, image, price }) {
+  const [isTrue, setIsTrue] = useState(true);
+  function handleStock() {
+    setIsTrue((isTrue) => !isTrue);
+  }
   return (
     <li className="card" data-testid="plant-item">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isTrue ? (
+        <button className="primary" onClick={handleStock}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStock}>Out of Stock</button>
       )}
     </li>
   );
